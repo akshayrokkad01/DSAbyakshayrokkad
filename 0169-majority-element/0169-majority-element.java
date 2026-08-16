b4603553418d5149acecd9adb3b378dda65563ef
Akshay Rokkadz R S <akshayrokkad54@gmail.com>
2026-08-16 20:23:16 +0530

    Time: 2 ms (70.95%), Space: 55.6 MB (46.86%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int majority = nums[0];
+        int votes = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (votes == 0) {
+                votes++; //to assign votes=1
+                majority = nums[i]; //assign new lead
+            }
+
+            else if (majority == nums[i]) //if same num = ++ ,diff num = --
+            {
+                votes++;
+            } else {
+                votes--;
+            }
+        }
+        return majority;
+    }
+}
