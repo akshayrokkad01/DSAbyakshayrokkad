afa44e3aa55f2e4d0313adae1d4669107d8a7aa3
Akshay Rokkad R S <akshayrokkad54@gmail.com>
2026-08-18 16:58:37 +0530

    Time: 4 ms (74.12%), Space: 45 MB (76.37%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,43 +1,29 @@
-import java.util.*;
-
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-
-        // Stores next greater element for each number in nums2
+        //to store element and next greatest element
         Map<Integer, Integer> map = new HashMap<>();
-
         Stack<Integer> stack = new Stack<>();
-
-        // Traverse nums2 from right to left
+        //traves from Right -->> left
         for (int i = nums2.length - 1; i >= 0; i--) {
-
             int element = nums2[i];
-
-            // Remove all elements smaller than or equal to current
+            // chech if stack is empty. or not and the peek value must be greater then element
             while (!stack.isEmpty() && stack.peek() <= element) {
                 stack.pop();
             }
-
-            // If stack is empty, no greater element exists
-            if (stack.isEmpty()) {
-                map.put(element, -1);
-            } 
-            else {
-                // Top of stack is the next greater element
-                map.put(element, stack.peek());
+            if(stack.isEmpty())
+            {
+                map.put(element,-1);
+            }else{
+                map.put(element,stack.peek());
             }
-
-            // Push current element
             stack.push(element);
         }
-
-        // Build answer for nums1
-        int[] result = new int[nums1.length];
-
-        for (int i = 0; i < nums1.length; i++) {
-            result[i] = map.get(nums1[i]);
+        // to get the NGE for nums1
+        int[] res=new int[nums1.length];
+        for(int i=0;i<nums1.length;i++)
+        {
+            res[i]=map.get(nums1[i]);
         }
-
-        return result;
+return res;
     }
 }
