3c8d18efec40295d52624e1b4eea0e193a3790c5
Akshay Rokkad R S <akshayrokkad54@gmail.com>
2026-08-19 13:13:43 +0530

    Time: 54 ms (80.46%), Space: 107.8 MB (23.54%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,22 +1,18 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        Stack<Integer> stack = new Stack<>();
         int n = temperatures.length;
-        int[] answer = new int[n]; // [0,0,0,0,0,0]
-        for (int i = n - 1; i >= 0; i--)// rigth to left.   left <<<<---- right
-        {
-            while (!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]) 
-            {
-                stack.pop(); // if it is >=stack.peek() we pop then it will be zero
+        int[] ans = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=0;i<n;i++){
+            while(!stack.isEmpty() && temperatures[i]>temperatures[stack.peek()]){
+                int index = stack.pop();
+                ans[index]=i-index;
             }
-            if (!stack.isEmpty()) {
-                answer[i] = stack.peek() - i; // i wil get stored in ans[] === [0,0,0,1,0,0] it will get updated to that current index
-            }
-
-
-            stack.push(i); // if satck isEmpty will push current index
-
+            stack.push(i);
         }
-        return answer;
+        return ans;
+        
     }
 }
