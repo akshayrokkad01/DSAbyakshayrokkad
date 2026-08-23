f14b787fef414233c206e941406e236a765d162b
Akshay Rokkad R S <akshayrokkad54@gmail.com>
2026-08-23 20:39:40 +0530

    Time: 1 ms (78.18%), Space: 42.5 MB (51.17%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isHappy(int n)
+     {
+        HashSet<Integer> usedSet=new HashSet<Integer>();
+        while(true)
+        {
+            //find squr for number
+            int sum=0;
+            while(n!=0){
+                sum+=Math.pow(n%10,2.0);
+                n=n/10;
+            }
+            if(sum==1)
+            {
+                return true;
+            }
+             n=sum;
+            if(usedSet.contains(n))
+            {
+                return false;
+            }
+            usedSet.add(n);
+        }
+    }
+}
